eead7f29bbdf841c1a7f1d56d365c7170aa61112
Natalia Rebecca Lin <natalialin2005@gmail.com>
2023-06-15 15:20:24 -0500

    Initial commit

--- src/main/java/com/example/tutorial/LoginController.java
+++ src/main/java/com/example/tutorial/LoginController.java
@@ -1,33 +1,50 @@
 package com.example.tutorial;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
 
 public class LoginController {
     public TextField cUsernameTxt;
 
     @FXML
-    public ObservableList<Login> login = FXCollections.observableArrayList();
+    public ObservableList<Login> username = FXCollections.observableArrayList();
     public Label wrongLogin;
 
 public void initialize() throws IOException {
-
+    loadUsername();
 }
     @FXML
+    private void loadUsername() throws FileNotFoundException {
+        //load username from saved file.
+        Gson gson = new Gson();
+        try (Reader reader = new FileReader("username.json")) {
+            ArrayList<Login> imports = gson.fromJson(reader, new TypeToken<ArrayList<Login>>() {
+            }.getType());
+            username = FXCollections.observableArrayList(imports);
+    } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
     private void checkLogin() throws IOException { //check username
-        if(cUsernameTxt.getText().equals(cUsernameTxt.getText())) {
+
+        if(cUsernameTxt.getText().equals(username.get(0).getUsername())) {
             wrongLogin.setText("Welcome!");
             StartApplication.setRoot("vocab-view");
-        }
-
-        else if(cUsernameTxt.getText().isEmpty()) {
+        } else if(cUsernameTxt.getText().isEmpty()) {
             wrongLogin.setText("Please enter a username."); //deny
-        }
-
-        else {
+        } else {
             wrongLogin.setText("Wrong username"); //deny
         }
     }
